c57161c00df190c29e3123e56d72857b0363b0cc
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-05-04 15:49:30 +0300

    types Props

--- components/QuestionCard.tsx
+++ components/QuestionCard.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-const QuestionCard = () => {
+type Props={
+    question:string;
+    answers:string[];
+    callback:any;
+    userAnswer:boolean
+    questionNumber:number
+    totalQuestion:number
+}
+
+const QuestionCard:React.FC<Props> = ({answers,callback,question,questionNumber,totalQuestion,userAnswer}) => {
   return (
-    <div>QuestionCard</div>
-  )
+    <div>
+      <p className="number">
+        Questin: {questionNumber} / {totalQuestion}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers?.map((answer) => (
+          <div key={answer}>
+            <button disabled={userAnswer} onClick={callback}>
+                <span dangerouslySetInnerHTML={{__html:answer}}/>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default QuestionCard
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Catamaran:wght@700&family=Fascinate+Inline&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
