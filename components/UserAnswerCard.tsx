3ea5c75b40ce469bb7baac089513812f527b6764
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-07 14:47:45 +0300

    answerpage-restartbutton

--- components/UserAnswerCard.tsx
+++ components/UserAnswerCard.tsx
@@ -1,43 +1,53 @@
 import { useContext } from "react";
 import { UserAnswersContext } from "../context/UserAnswersProvider";
 import { ButtonWrapper, Wrapper, Flex } from "./UserAnswerCard.styles";
+import { useRouter } from "next/router";
 
 const UserAnswerCard = () => {
-
+    const router = useRouter();
     const { userAnswers } = useContext(UserAnswersContext);
+
+    const handleGoBack = () => {
+      router.back(); // Bir önceki sayfaya yönlendirir
+    };
 
     console.log(userAnswers);
 
   return (
-    <Flex>
-      {userAnswers.map((item, i) => (
-        <Wrapper key={i}>
-          <p>
-            {" "}
-            {i + 1} - {item.question}{" "}
-          </p>
-          <div>
-            <ButtonWrapper correct={item.correct}>
-              <button value={item.answer}>
-                <span dangerouslySetInnerHTML={{ __html: item.answer }} />
-              </button>
-            </ButtonWrapper>
-            <ButtonWrapper correct={item.correct}>
-              <button
-                value={item.correctAnswer}
-                style={{
-                  background: "linear-gradient(90deg, #56FFA4, #59BC86)",
-                }}
-              >
-                <span
-                  dangerouslySetInnerHTML={{ __html: item.correctAnswer }}
-                />
-              </button>
-            </ButtonWrapper>
-          </div>
-        </Wrapper>
-      ))}
-    </Flex>
+    <div className="correctAnswer">
+      <Flex>
+        {userAnswers.map((item, i) => (
+          <Wrapper key={i}>
+            <p>
+              {" "}
+              {i + 1} - {item.question}{" "}
+            </p>
+            <div>
+              <ButtonWrapper correct={item.correct}>
+                <button value={item.answer}>
+                  <span dangerouslySetInnerHTML={{ __html: item.answer }} />
+                </button>
+              </ButtonWrapper>
+              <ButtonWrapper correct={item.correct}>
+                <button
+                  value={item.correctAnswer}
+                  style={{
+                    background: "linear-gradient(90deg, #56FFA4, #59BC86)",
+                  }}
+                >
+                  <span
+                    dangerouslySetInnerHTML={{ __html: item.correctAnswer }}
+                  />
+                </button>
+              </ButtonWrapper>
+            </div>
+          </Wrapper>
+        ))}
+      </Flex>
+      <button className="show" onClick={handleGoBack}>
+        Restart
+      </button>
+    </div>
   );
 };
 
